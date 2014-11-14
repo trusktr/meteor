0a81dc5a2ba1773f65e2b8fa5bfc19575e6700a1
Ben Newman <bn@cs.stanford.edu>
2014-11-14 16:56:13 -0500

    Implement funcUtils.coalesce(delayMs, callback).

--- tools/func-utils.js
+++ tools/func-utils.js
@@ -1,4 +1,5 @@
 var _ = require("underscore");
+var fiberHelpers = require("./fiber-helpers.js");
 
 function wrap(wrapper, wrapped) {
   // Allow the wrapper to be used as a constructor function, just in case
@@ -57,4 +58,52 @@ exports.constructLazyInstance = function(constructor/*, arg1, arg2, ... */) {
   });
 
   return instance;
+};
+
+// Return a function that coalesceses calls to fn that occur within delay
+// milliseconds of each other, and prevents overlapping invocations of fn
+// by postponing the next invocation until after fn's fiber finishes.
+exports.coalesce = function(delayMs, callback, context) {
+  var pendingTimer = null;
+  var inProgress = 0;
+
+  delayMs = delayMs || 100;
+
+  function coalescingWrapper() {
+    var self = context || this;
+
+    if (inProgress) {
+      // Indicate that coalescingWrapper should be called again after the
+      // callback is no longer in progress.
+      ++inProgress;
+      return;
+    }
+
+    if (pendingTimer !== null) {
+      // Defer to the already-pending timer.
+      return;
+    }
+
+    var fiberCallback = fiberHelpers.inBareFiber(function() {
+      // Now that the timeout has fired, set inProgress to 1 so that
+      // (until the callback is complete and we set inProgress to 0 again)
+      // any calls to coalescingWrapper will increment inProgress to
+      // indicate that at least one other caller wants fiberCallback to be
+      // called again when the original callback is complete.
+      pendingTimer = null;
+      inProgress = 1;
+
+      try {
+        callback.call(self);
+      } finally {
+        if (inProgress > 1)
+          pendingTimer = setTimeout(fiberCallback, 10);
+        inProgress = 0;
+      }
+    });
+
+    pendingTimer = setTimeout(fiberCallback, delayMs);
+  }
+
+  return wrap(coalescingWrapper, callback);
 };
