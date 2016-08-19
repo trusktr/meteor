5b9236bc1d25177512467ad4649e802e658c8d49
Joseph Orbegoso Pea <joe@trusktr.io>
2016-08-18 18:42:39 -0700

    better example

--- meteor-app/client/main.js
+++ meteor-app/client/main.js
@@ -1,32 +1,8 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import startup from 'awaitbox/meteor/startup'
-//
-// import './main.html';
-//
-// import App from './app/App'
-//
-// main()
-// async function main() {
-//     await startup()
-//     const appRoot = document.querySelector('#app-root')
-//     ReactDOM.render(<App msg="Mighty Devs!" />, appRoot)
-// }
-
 class A {
-    static [Symbol.hasInstance] (instance) {
-        console.log('custom instanceof check!', instance)
-        return false
+    static [Symbol.hasInstance](obj) {
+        if (this !== A) return super[Symbol.hasInstance](obj)
+        return true
     }
 }
 
-console.log(A[Symbol.hasInstance]) // it is there!
-
-class B extends A {}
-
-class C extends B {}
-class D extends B {}
-
-console.log('should be false:', (new C) instanceof D)
-console.log('should be true:', (new C) instanceof B)
-console.log('should be false:', (new C) instanceof A) // outputs "true", and should also output "custom instanceof check!" but doesn't.
+console.log('What the?', Object.getOwnPropertySymbols(A))
