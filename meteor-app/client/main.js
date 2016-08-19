b45c5f04b7a388bc7a7bff70560ddf6bff428100
Joseph Orbegoso Pea <joe@trusktr.io>
2016-08-18 19:47:56 -0700

    Added working example.

--- meteor-app/client/main.js
+++ meteor-app/client/main.js
@@ -1,3 +1,4 @@
+// doesn't work
 class A {
     static [Symbol.hasInstance](obj) {
         if (this !== A) return super[Symbol.hasInstance](obj)
@@ -6,3 +7,16 @@ class A {
 }
 
 console.log('What the?', Object.getOwnPropertySymbols(A))
+
+// works
+class B {
+}
+
+Object.defineProperty(B, Symbol.hasInstance, {
+    value: function (obj) {
+        if (this !== B) return Object.getPrototypeOf(B)[Symbol.hasInstance].call(this, obj)
+        return true
+    }
+})
+
+console.log('What the?', Object.getOwnPropertySymbols(B))
